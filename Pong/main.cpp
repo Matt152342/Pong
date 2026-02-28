20d552700191e0afb896f68eb166e29f78525967
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-02-28 15:50:40 +0200

    Created windows template

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,1 +1,24 @@
 #include "raylib.h"
+
+#include <iostream>
+#include <string>
+
+const int screenWidth = 800;
+const int screenHeight = 500;
+
+int main() {
+	InitWindow(screenWidth, screenHeight, "Pong");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		// Update
+
+		// Drawing
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
